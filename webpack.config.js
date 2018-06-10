cca4472a414ebf511ae158f26094f9314ed23e90
xiepei <1005039110@qq.com>
2018-06-10 12:26:19 +0800

    配置webpack

--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,11 @@ module.exports = {
   },
   mode: 'development',
   devServer: {
-    port: 8080, // 设置监听端口（默认的就是8080）
+    port: 8080,
     hot: true,
     inline: true,
     publicPath: '/',
-    contentBase: "./build", // 本地服务器所加载的页面所在的目录
-    historyApiFallback: true // 不跳转，用于开发单页面应用，依赖于HTML5 history API 设置为true点击链接还是指向index.html
+    contentBase: "./build",
+    historyApiFallback: true
   }
 }
